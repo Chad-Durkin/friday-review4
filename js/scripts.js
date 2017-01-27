9d9801209ca5abe74a355ca71e3a30b165b73b14
Chad Durkin <Chaddurkin@gmail.com>
2017-01-27 11:45:37 -0800

    add input functionality, using a prototype, the back-end now stores the useres pizza options in an array and each time they make a new order it stores the last order

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,79 @@
-
-function Pizza(pizzaSize, pizzaDough, pizzaCrust, pizzaToppings)
-{
+function Pizza(pizzaSize, pizzaDough, pizzaCrust) {
   this.pizzaSize = pizzaSize;
   this.pizzaDough = pizzaDough;
   this.pizzaCrust = pizzaCrust;
-  this.pizzaToppings = pizzaToppings
-}
+  this.pizzaToppings = [];
+  this.pizzaCost = 0;
+};
 
-function addToppings(pizzaToppingsArray){
+Pizza.prototype = {
+  cost: function() {
+  if(this.pizzaSize === "small") {
+    if(this.pizzaToppings.length <= 3) {
+      this.pizzaCost += 8;
+    } else if(this.pizzaToppings.length <= 5) {
+      this.pizzaCost += 12;
+    } else if(this.pizzaToppings.length > 5 ) {
+      this.pizzaCost += 16;
+    }
+  } else if(this.pizzaSize === "medium") {
+      if(this.pizzaToppings.length <= 3) {
+        this.pizzaCost += 12.00;
+      } else if(this.pizzaToppings.length <= 5) {
+        this.pizzaCost += 16.00;
+      } else if(this.pizzaToppings.length >5 ) {
+        this.pizzaCost += 20.00;
+      }
+    } else if(this.pizzaSize === "large") {
+        if(this.pizzaToppings.length <= 3) {
+          this.pizzaCost += 16.00;
+        } else if(this.pizzaToppings.length <= 5) {
+          this.pizzaCost += 20.00;
+        } else if(this.pizzaToppings.length >5 ) {
+          this.pizzaCost += 24.00;
+        }
+      } else if(this.pizzaSize === "extraLarge") {
+          if(this.pizzaToppings.length <= 3) {
+            this.pizzaCost += 20.00;
+          } else if(this.pizzaToppings.length <= 5) {
+            this.pizzaCost += 24.00;
+          } else if(this.pizzaToppings.length >5 ) {
+            this.pizzaCost += 28.00;
+          }
+        }
+    if(this.pizzaDough === "wheat") {
+      this.pizzaCost += 3;
+    }
+    if(this.pizzaCrust === "extraThick") {
+      this.pizzaCost += 3;
+    } else if(this.pizzaCrust === "thin") {
+      this.pizzaCost += 3;
+    }
+  }
+};
+
+function addToppings(){
+var pizzaToppingsArray = [];
   for (var index = 0; index <= 14; index++) {
     if(typeof $("input:checkbox[name=top" + index + "]:checked").val() != "undefined") {
       pizzaToppingsArray.push($("input:checkbox[name=top" + index + "]:checked").val());
     }
   }
   return pizzaToppingsArray;
-}
+};
 
 $(function() {
-var pizzaToppingsArray = [];
+var pizzaOrders = [];
   $("#pizza-input").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("#pizza-size option:selected").val();
     var pizzaDough = $("#pizza-dough option:selected").val();
     var pizzaCrust = $("#pizza-crust option:selected").val();
-    pizzaToppingsArray = addToppings(pizzaToppingsArray);
+    var newPizza = new Pizza(pizzaSize, pizzaDough, pizzaCrust);
+    newPizza.pizzaToppings = addToppings();
+    newPizza.cost();
+    console.log(newPizza);
+    pizzaOrders.push(newPizza);
+    console.log(pizzaOrders);
     })
 });
